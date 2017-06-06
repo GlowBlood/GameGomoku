0ba5a9e88c76687b62432eae259e61a77a695b79
GlowBlood <ipberezovskaya@yandex.ru>
2017-06-06 12:00:06 +0300

    ...

--- src/newGame/gamePanel.java
+++ src/newGame/gamePanel.java
@@ -1,18 +1,67 @@
 package newGame;
 
-import java.awt.Color;
-import java.awt.Graphics;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.JPanel;
 
 @SuppressWarnings("WeakerAccess")
 
 public class gamePanel extends JPanel {
 
-    public gamePanel() {
-        setBackground(new Color(160,82,45));
+//массивы координат центров шариков
+    private int[] massivX = {50, 100, 150, 200, 250, 300, 350, 400, 450};
+    private int[] massivY = {50, 100, 150, 200, 250, 300, 350, 400, 450};
+
+//проверка правильности щелчка игрока
+    private Boolean isInMassive(int x, int y) {
+        for (int i = 0; i <massivX.length; i++){
+            for (int j=0; j<massivY.length; i++){
+                Double ifX = Math.pow(2, (i - x));
+                Double ifY = Math.pow(2, (j - y));
+                if (ifX + ifY <= 400.0) return true;
+            }
+        }
+        return false;
+    }
+//нахождение Х координаты нужного центра шарика
+    private Integer ballX(int x, int y) {
+        int result = 0;
+        if (isInMassive(x, y)) {
+            for (int i = 0; i < massivX.length; i++) {
+                for (int j = 0; j < massivY.length; i++) {
+                    Double ifX = Math.pow(2, (i - x));
+                    Double ifY = Math.pow(2, (j - y));
+                    if (ifX + ifY <= 400.0) result = i;
+                }
+            }
+        }
+        return result;
+    }
+//нахождение У координаты нужного центра шарика
+    private Integer ballY(int x, int y) {
+        int result = 0;
+        if (isInMassive(x, y)) {
+
+            for (int i = 0; i < massivX.length; i++) {
+                for (int j = 0; j < massivY.length; i++) {
+                    Double ifX = Math.pow(2, (i - x));
+                    Double ifY = Math.pow(2, (j - y));
+                    if (ifX + ifY <= 400.0) result = j;
+                }
+            }
+        }
+        return result;
     }
 
-@Override
+
+
+    public gamePanel() {
+        setBackground(new Color(160,82,45));
+
+    }
+
     public void paintComponent(Graphics g) {
         g.setColor(Color.black);
         g.drawLine(50,0,50,500);
@@ -34,9 +83,67 @@ public class gamePanel extends JPanel {
         g.drawLine(0,350, 500,350);
         g.drawLine(0,400, 500,400);
         g.drawLine(0,450, 500,450);
+    }
+
+    private void paintBall(Graphics g, Ball b){
+
+        g.setColor(b.getColor());
+        int radius = b.getRadius();
+        g.fillOval(b.getX() - radius, b.getY() - radius, 2*radius, 2*radius);
 
     }
 
+    class Ball {
+        int x;
+        int y;
+        int radius = 20;
+        Color color;
 
+        public void setColor(Color color){
+            this.color = color;
+        }
+
+        public void setX(Integer x){
+            this.x = x;
+        }
+
+        public void setY(Integer y){
+            this.y = y;
+        }
+
+        public Color getColor(){
+            return color;
+        }
+
+        public Integer getRadius(){
+            return radius;
+        }
+        
+        public Integer getX(){
+            return x;
+        }
+
+        public Integer getY(){
+            return y;
+        }
+    }
+
+    public void init() {
+        addMouseListener((MouseListener) this);
+
+    }
+
+    public void mouseClicked(MouseEvent event) {
+        int x = event.getX();
+        int y = event.getY();
+        int finalX = ballX(x, y);
+        int finalY = ballY(x, y);
+        Ball ball = new Ball();
+        ball.setColor(Color.CYAN);
+        ball.setX(finalX);
+        ball.setY(finalY);
+
+        paintBall(getGraphics(), ball );
+    }
 
 }
--- src/newGame/newGame.java
+++ src/newGame/newGame.java
@@ -15,8 +15,6 @@ import static com.sun.java.accessibility.util.AWTEventMonitor.addMouseMotionList
 
 
 public class newGame extends JFrame {
-
-
     private newGame(String s) {
         super(s);
         setSize(500, 600);
@@ -24,7 +22,6 @@ public class newGame extends JFrame {
 
 
         JPanel gamePanel = new gamePanel();
-        setBackground(new Color(160,82,45));
         gamePanel.setBounds(0,0,500,500);
         add(gamePanel);
 
@@ -42,108 +39,13 @@ public class newGame extends JFrame {
         panel.setBackground(Color.black);
         add(panel);
 
-
         setVisible(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-
 
     }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new newGame("Gomoku"));
-    }
-
-
-    public  class  MouseEvents implements MouseListener {
-
-        private int[] massivX = {50, 100, 150, 200, 250, 300, 350, 400, 450};
-        private int[] massivY = {50, 100, 150, 200, 250, 300, 350, 400, 450};
-
-        private Boolean isInMassive(int x, int y) {
-            for (int i = 0; i <massivX.length; i++){
-                for (int j=0; j<massivY.length; i++){
-                    Double ifX = Math.pow(2, (i - x));
-                    Double ifY = Math.pow(2, (j - y));
-                    if (ifX + ifY <= 400.0) return true;
-                }
-            }
-            return false;
-        }
-
-        public Integer ballX(int x, int y) {
-            int result = 0;
-            if (isInMassive(x, y) == true) {
-
-                for (int i = 0; i < massivX.length; i++) {
-                    for (int j = 0; j < massivY.length; i++) {
-                        Double ifX = Math.pow(2, (i - x));
-                        Double ifY = Math.pow(2, (j - y));
-                        if (ifX + ifY <= 400.0) result = i;
-                    }
-                }
-            }
-            return result;
-        }
-
-        public Integer ballY(int x, int y) {
-            int result = 0;
-            if (isInMassive(x, y) == true) {
-
-                for (int i = 0; i < massivX.length; i++) {
-                    for (int j = 0; j < massivY.length; i++) {
-                        Double ifX = Math.pow(2, (i - x));
-                        Double ifY = Math.pow(2, (j - y));
-                        if (ifX + ifY <= 400.0) result = j;
-                    }
-                }
-            }
-            return result;
-        }
-
-
-        public void init() {
-            // Инициализация интерфейсов
-            addMouseListener(this);
-
-        }
-
-        // Метод обработки щелчка мышки
-        public int finalX = 0;
-        public int finalY = 0;
-
-        public void mouseClicked(MouseEvent event) {
-
-            int x = event.getX();
-            int y = event.getY();
-            finalX = ballX(x, y);
-            finalY = ballY(x, y);
-        }
-
-        public void mouseExited(MouseEvent event){
-        }
-
-        public void mouseEntered(MouseEvent e) {
-        }
-
-        // Метод обработки ввода мышки в обьект
-        public void mouseEntemaroon(MouseEvent event) {
-        }
-
-        // Метод обработки нажатия кнопки мышки в обьекте
-        public void mousePressed(MouseEvent event) {
-        }
-
-        // Метод обработки освобождения кнопки  мышки в обьекте
-        public void mouseReleased(MouseEvent event) {
-        }
-        // Метод обработки движения "нажатой" мышки в обьекте
-        public void mouseDragged(MouseEvent event) {
-        }
-        // Метод обработки движения мышки в обьекте
-        public void mouseMoved(MouseEvent event) {
-        }
-
     }
 
 }
